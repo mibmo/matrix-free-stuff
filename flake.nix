1a4eb11c6315beb2f7e920a9c30d3481fb337afb
mib <mib@mib.dev>
2024-03-20 22:43:47 +0100

    appservice registration

--- flake.nix
+++ flake.nix
@@ -1,23 +1,135 @@
 {
+  description = "Build a cargo project";
+
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-    conch = {
-      url = "github:mibmo/conch";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane = {
+      url = "github:ipetkov/crane";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-analyzer-src.follows = "";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
     };
   };
 
-  outputs = { conch, ... }:
-    conch.load [
-      "aarch64-darwin"
-      "riscv64-linux"
-      "x86_64-darwin"
-      "x86_64-linux"
-    ]
-      ({ ... }: {
-        development.rust = {
-          enable = true;
-          profile = "stable";
+  outputs = { self, nixpkgs, crane, fenix, flake-utils, rust-overlay, advisory-db, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+
+        inherit (pkgs) lib;
+
+        craneLib = (crane.mkLib pkgs).overrideToolchain pkgs.rust-bin.nightly.latest.default;
+        src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+
+          buildInputs = with pkgs; [
+            pkg-config
+            openssl
+          ] ++ lib.optionals pkgs.stdenv.isDarwin [
+            libiconv
+          ];
+        };
+
+        craneLibLLvmTools = craneLib.overrideToolchain
+          (fenix.packages.${system}.complete.withComponents [
+            "cargo"
+            "llvm-tools"
+            "rustc"
+          ]);
+
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        matrix-free-stuff = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+        });
+      in
+      {
+        checks = {
+          # Build the crate as part of `nix flake check` for convenience
+          inherit matrix-free-stuff;
+
+          # Run clippy (and deny all warnings) on the crate source,
+          # again, reusing the dependency artifacts from above.
+          #
+          # Note that this is done as a separate derivation so that
+          # we can block the CI if there are issues here, but not
+          # prevent downstream consumers from building our crate by itself.
+          matrix-free-stuff-clippy = craneLib.cargoClippy (commonArgs // {
+            inherit cargoArtifacts;
+            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+          });
+
+          matrix-free-stuff-doc = craneLib.cargoDoc (commonArgs // {
+            inherit cargoArtifacts;
+          });
+
+          # Check formatting
+          matrix-free-stuff-fmt = craneLib.cargoFmt {
+            inherit src;
+          };
+
+          # Audit dependencies
+          matrix-free-stuff-audit = craneLib.cargoAudit {
+            inherit src advisory-db;
+          };
+
+          # Audit licenses
+          matrix-free-stuff-deny = craneLib.cargoDeny {
+            inherit src;
+          };
+
+          # Run tests with cargo-nextest
+          # Consider setting `doCheck = false` on `matrix-free-stuff` if you do not want
+          # the tests to run twice
+          matrix-free-stuff-nextest = craneLib.cargoNextest (commonArgs // {
+            inherit cargoArtifacts;
+            partitions = 1;
+            partitionType = "count";
+          });
+        };
+
+        packages = {
+          default = matrix-free-stuff;
+          inherit matrix-free-stuff;
+        } // lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
+          matrix-free-stuff-llvm-coverage = craneLibLLvmTools.cargoLlvmCov (commonArgs // {
+            inherit cargoArtifacts;
+          });
+        };
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = matrix-free-stuff;
+        };
+
+        devShells.default = craneLib.devShell {
+          checks = self.checks.${system};
+          packages = with pkgs; [ ];
         };
       });
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(absolute_path)]
+
 use axum::{
     extract::State,
     http::StatusCode,
@@ -5,11 +7,20 @@ use axum::{
     routing::{get, post},
     Json, Router,
 };
+use eyre::Result as EResult;
 use freestuffapi::api::GameId;
+use rand::{distributions, Rng};
+use ruma::api::appservice::{Namespaces, Registration, RegistrationInit};
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use serde_json::Value as JsonValue;
-use eyre::Result as EResult;
 use tracing::*;
+
+use std::fs::{create_dir_all, File};
+use std::io::Write;
+use std::path::{Path, PathBuf};
+
+const APPSERVICE_ID: &'static str = "matrix-free-stuff";
+const TOKEN_LENGTH: usize = 20;
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
 struct ApiSecret(pub String);
@@ -18,6 +29,100 @@ struct ApiSecret(pub String);
 #[instrument]
 async fn main() -> EResult<()> {
     tracing_subscriber::fmt::init();
+
+    let homeserver_url = std::env::var("HOMESERVER_URL")
+        .expect("required environment variable HOMESERVER_URL not set");
+
+    let registration: Registration = match std::env::var("APPSERVICE_REGISTRATION")
+        .map(PathBuf::from)
+        .map(|path| (path.clone(), File::open(&path)))
+    {
+        Ok((_, Ok(file))) => {
+            debug!("loading registration from file");
+            serde_yaml::from_reader(file).expect("failed to deserialize registration")
+        }
+        Ok((path, Err(err))) if err.kind() == std::io::ErrorKind::NotFound => {
+            debug!(?err, "failed to open registration file");
+            warn!("registration file at path is empty; creating new registration in it's place");
+
+            let absolute_path = std::path::absolute(&path)
+                .expect("could not get absolute path of registration file");
+            let dir_path = absolute_path.parent().unwrap_or(Path::new("."));
+
+            info!(?dir_path, "creating leading directories");
+            if let Err(err) = create_dir_all(dir_path) {
+                error!(?err, ?dir_path, "failed to create leading directories");
+            }
+
+            match File::create(&absolute_path) {
+                Ok(mut file) => {
+                    let mut rng = rand::thread_rng();
+
+                    let mut random_string = |length: usize| -> String {
+                        (0..length)
+                            .map(|_| rng.sample(distributions::Alphanumeric) as char)
+                            .collect()
+                    };
+
+                    let as_token = random_string(TOKEN_LENGTH);
+                    let hs_token = random_string(TOKEN_LENGTH);
+
+                    let registration: Registration = RegistrationInit {
+                        id: APPSERVICE_ID.to_string(),
+                        url: String::new(),
+                        as_token,
+                        hs_token,
+                        sender_localpart: "free-stuff".to_string(),
+                        namespaces: Namespaces::new(),
+                        rate_limited: None,
+                        protocols: None,
+                    }
+                    .into();
+
+                    let serialized = registration
+                        .serialize(serde_yaml::value::Serializer)
+                        .and_then(|x| serde_yaml::to_string(&x))
+                        .expect("failed to serialize registration");
+
+                    match file.write_all(&serialized.into_bytes()) {
+                        Ok(_) => info!(?absolute_path, "created registration file"),
+                        Err(err) => {
+                            error!(?err, ?absolute_path, "failed to write registration file")
+                        }
+                    }
+
+                    registration.into()
+                }
+                Err(err) => {
+                    error!(?err, ?absolute_path, "could not create registration file");
+                    std::process::exit(1);
+                }
+            }
+        }
+        Ok((path, Err(err))) => {
+            error!(
+                ?err,
+                ?path,
+                "failed to open existing registration file. exiting"
+            );
+            std::process::exit(1);
+        }
+        Err(err) => {
+            debug!(
+                ?err,
+                "could not get environment variable APPSERVICE_REGISTRATION"
+            );
+            error!(
+                "no path to registration specified; please set APPSERVICE_REGISTRATION. exiting"
+            );
+            std::process::exit(1);
+        }
+    };
+
+    let client = ruma::client::Client::builder()
+        .homeserver_url(homeserver_url)
+        .build::<ruma::client::http_client::HyperNativeTls>()
+        .await?;
 
     let webhook_path = std::env::var("WEBHOOK_PATH")
         .map_err(|_| debug!("no webhook path specified"))
@@ -49,7 +154,6 @@ struct Event {
     secret: Option<String>,
     data: JsonValue,
 }
-
 
 enum EventError {
     Json(serde_json::Error),
